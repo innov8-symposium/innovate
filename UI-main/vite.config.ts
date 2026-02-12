5a620ee08a0f205be7270814e5d02eb069e4dbe7
innov8-symposium <nammafacts1427@gmail.com>
2026-02-13 02:05:30 +0530

    first commit

--- UI-main/vite.config.ts
+++ UI-main/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/UI/", 
+  base: "/innovate/", 
   server: {
     host: "::",
     port: 8080,
